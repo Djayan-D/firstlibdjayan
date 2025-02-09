66fcb2d2b2bf9bf2e2c2d0b9bf5e206a17b4079e
Djayan-D <daeron.djayan@gmail.com>
2025-02-09 18:26:30 +0100

    Implémentation fonction creer_departement

---
+++ R/creer_departement.R
@@ -0,0 +1,32 @@
+#' Créer un objet de classe "departement"
+#'
+#' Cette fonction permet de vérifier si le `data.frame` contient les données d'un seul département. Si le `data.frame`
+#' contient plusieurs départements, la fonction arrête l'exécution avec un message d'erreur. Si un seul département est présent,
+#' elle ajoute la classe "departement" au `data.frame`.
+#'
+#' @param df Un `data.frame` contenant les données des départements, avec la colonne `Code.du.département` utilisée
+#' pour vérifier si plusieurs départements sont présents dans le `data.frame`.
+#' @return Un `data.frame` auquel la classe "departement" est ajoutée s'il contient un seul département.
+#' @throws Error si le `data.frame` contient plusieurs départements (c'est-à-dire plusieurs valeurs distinctes pour `Code.du.département`).
+#' @export
+
+creer_departement <- function(df) {
+  # Vérifier si le DataFrame est composé de plusieurs départements
+
+  unique_departments <- df$Code.du.département |>
+    unique() |>
+    length()
+
+  if (unique_departments > 1) {
+    stop("Le DataFrame est composé de plusieurs départements.") # Arrête la fonction s'il y a plusieurs départements
+  }
+
+
+  # Ajouter au DataFrame la classe "departement" si elle est absente
+
+  if (!inherits(df, "departement")) {
+    class(df) <- c("departement", class(df)) # Ajoute la classe "departement" si elle n'est pas présente
+  }
+
+  return(df)
+}
