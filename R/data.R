4789de74426aec3082f340d8b523f36c8f47bcc4
Djayan-D <daeron.djayan@gmail.com>
2025-02-09 20:22:58 +0100

    Ajout du jeu de données

---
+++ R/data.R
@@ -0,0 +1,26 @@
+#' Données des élus
+#'
+#' Un jeu de données contenant des informations sur les élus des départements Loire-Atlantique, Gers, ainsi que de la commune de Faverelles dans le Loiret.
+#'
+#' @format ## `elus_sample`
+#' Un data frame avec 9 752 lignes et 16 colonnes :
+#' \describe{
+#'   \item{Code.du.département}{Code unique identifiant le département (ex : 44 pour Loire-Atlantique, 32 pour Gers)}
+#'   \item{Libellé.du.département}{Nom du département (ex : "Loire-Atlantique", "Gers", ou "Loiret")}
+#'   \item{Code.de.la.collectivité.à.statut.particulier}{Code identifiant une collectivité ayant un statut particulier}
+#'   \item{Libellé.de.la.collectivité.à.statut.particulier}{Nom de la collectivité à statut particulier}
+#'   \item{Code.de.la.commune}{Code unique identifiant la commune (ex : 44000 pour Nantes, 32000 pour Auch, ou code de Faverelles dans le Loiret)}
+#'   \item{Libellé.de.la.commune}{Nom de la commune (ex : "Nantes", "Auch", ou "Faverelles")}
+#'   \item{Nom.de.l.élu}{Nom de l'élu}
+#'   \item{Prénom.de.l.élu}{Prénom de l'élu}
+#'   \item{Code.sexe}{Code indiquant le sexe de l'élu (ex : M pour masculin, F pour féminin)}
+#'   \item{Date.de.naissance}{Date de naissance de l'élu}
+#'   \item{Code.de.la.catégorie.socio.professionnelle}{Code de la catégorie socio-professionnelle de l'élu}
+#'   \item{Libellé.de.la.catégorie.socio.professionnelle}{Libellé de la catégorie socio-professionnelle de l'élu}
+#'   \item{Date.de.début.du.mandat}{Date de début du mandat de l'élu}
+#'   \item{Libellé.de.la.fonction}{Fonction exercée par l'élu (ex : maire, conseiller municipal)}
+#'   \item{Date.de.début.de.la.fonction}{Date de début de la fonction de l'élu}
+#'   \item{Code.nationalité}{Code de la nationalité de l'élu}
+#' }
+#' @source <URL ou source de vos données>
+"elus_sample"
