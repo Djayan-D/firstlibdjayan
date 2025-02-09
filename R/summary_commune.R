e12a9290017c509ff31f1f88c95ee673740bb6fa
Djayan-D <daeron.djayan@gmail.com>
2025-02-09 18:26:46 +0100

    Implémentation fonction summary_commune

---
+++ R/summary_commune.R
@@ -0,0 +1,45 @@
+#' Résumé des informations d'une commune
+#'
+#' Cette fonction génère un résumé des informations pour une commune donnée. Elle vérifie que l'objet passé en argument est bien
+#' de la classe "commune", puis retourne une liste contenant :
+#' - Le nom de la commune,
+#' - Le nombre d'élus,
+#' - La distribution des âges des élus,
+#' - L'élu le plus âgé.
+#'
+#' @param x Un objet de classe `commune`, qui est un `data.frame` contenant les données des élus d'une commune.
+#' @return Une liste contenant :
+#' - `nom_commune` : Le nom de la commune,
+#' - `nombre_elus` : Le nombre d'élus dans la commune,
+#' - `distribution_ages_elus` : Les quantiles de la distribution des âges des élus,
+#' - `elu_plus_age` : Les informations de l'élu le plus âgé.
+#' @throws Error si l'objet `x` n'est pas de la classe "commune".
+#' @export
+
+summary.commune <- function(x) {
+  # Vérifier que le DataFrame respecte la structure minimale
+
+  validate_schema(x)
+
+
+  # Vérifier que l'objet x est bien de la classe "commune"
+
+  if (!inherits(x, "commune")) {
+    stop("L'objet doit être de classe 'commune'")
+  }
+
+
+  # Créer la liste des informations de la commune
+
+  info_commune <- list(
+    nom_commune = unique(x$Libellé.de.la.commune),
+    nombre_elus = compter_nombre_d_elus(x),
+    distribution_ages_elus = calcul_distribution_age(x),
+    elu_plus_age = trouver_l_elu_le_plus_age(x)
+  )
+
+
+  # Retourner les informations
+
+  return(info_commune)
+}
