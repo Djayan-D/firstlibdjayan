eb9c545bc5a6637db3765c8a60ce26069b45784d
Djayan-D <daeron.djayan@gmail.com>
2025-02-14 22:57:11 +0100

    test réglage problème concordance numéro et nom département / commune

--- R/generer_rapport.R
+++ R/generer_rapport.R
@@ -2,8 +2,8 @@
 #'
 #' Cette fonction génère un rapport au format PDF à partir d'un fichier Quarto (.qmd) en utilisant les paramètres de la commune et du département fournis.
 #'
-#' @param commune Un caractère représentant le nom de la commune pour laquelle le rapport sera généré.
-#' @param departement Un caractère représentant le nom du département pour lequel le rapport sera généré.
+#' @param commune Le code (ex : INSEE) associé à la commune pour laquelle le rapport sera généré.
+#' @param departement Le code (ex : INSEE) associé au département pour lequel le rapport sera généré.
 #' @param output Un caractère spécifiant le chemin du fichier de sortie (par exemple, "rapport_nantes.pdf").
 #'
 #' @return Aucun. La fonction génère un fichier de rapport au format spécifié (par défaut PDF).
@@ -12,7 +12,7 @@
 #' @export
 #'
 #' @examples
-#' generer_rapport("Nantes", "Loire-Atlantique", "rapport_nantes.pdf")
+#' generer_rapport("44109", "Loire-44", "rapport_Nantes_et_Loire_Atlantique.pdf")
 
 generer_rapport <- function(commune, departement, output) {
   # Localiser le fichier .qmd dans le dossier inst
@@ -21,7 +21,7 @@ generer_rapport <- function(commune, departement, output) {
   # Générer le rapport à l'aide de Quarto
   quarto::quarto_render(input = qmd_path,
                         output_file = output,
-                        params = list(commune = commune, departement = departement))
+                        params = list(code_commune = commune, code_departement = departement))
 
   # Renvoyer un message pour confirmer la génération
   message("Le rapport a été généré avec succès.")
