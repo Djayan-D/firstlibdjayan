0d67c8bd1da1f36072f33d26f35c46f9b6da67d1
Djayan-D <daeron.djayan@gmail.com>
2025-02-24 13:13:11 +0100

    réglage fonction

--- R/generer_rapport.R
+++ R/generer_rapport.R
@@ -4,21 +4,14 @@
 #'
 #' @param commune Le code (ex : INSEE) associé à la commune pour laquelle le rapport sera généré.
 #' @param departement Le code (ex : INSEE) associé au département pour lequel le rapport sera généré.
-<<<<<<< HEAD
 #' @param output Un caractère spécifiant le chemin du fichier de sortie (par exemple, "rapport_nantes.html").
 #'
 #' @return Aucun. La fonction génère un fichier de rapport au format spécifié (par défaut html).
-=======
-#' @param output Un caractère spécifiant le chemin du fichier de sortie (par exemple, "rapport_nantes.pdf").
-#'
-#' @return Aucun. La fonction génère un fichier de rapport au format spécifié (par défaut PDF).
->>>>>>> gh-pages
 #'
 #' @import quarto
 #' @export
 #'
 #' @examples
-<<<<<<< HEAD
 #' generer_rapport(44109, 44, "rapport_Nantes_et_Loire_Atlantique.html")
 
 generer_rapport <- function(commune, departement, output, df = elus_sample) {
@@ -43,21 +36,11 @@ generer_rapport <- function(commune, departement, output, df = elus_sample) {
   }
 
 
-
-
-
   # Déterminer le chemin du fichier Quarto
-=======
-#' generer_rapport(44109, 44, "rapport_Nantes_et_Loire_Atlantique.pdf")
-
-generer_rapport <- function(commune, departement, output) {
-  # Localiser le fichier .qmd dans le dossier inst
->>>>>>> gh-pages
 
   qmd_path <- system.file("rapport.qmd", package = "firstlibdjayan")
 
 
-<<<<<<< HEAD
   # Exécuter le rendu du rapport avec Quarto
 
   quarto::quarto_render(
@@ -71,23 +54,7 @@ generer_rapport <- function(commune, departement, output) {
       dataframe_path = save_path
     )
   )
-=======
-  # Générer le rapport à l'aide de Quarto
-  quarto::quarto_render(input = qmd_path,
-                        output_format = "html",
-                        output_file = output,
-                        execute_params = list(code_commune = commune,
-                                              code_departement = departement))
 
-
-  # Renvoyer un message pour confirmer la génération
->>>>>>> gh-pages
 
   message("Le rapport a été généré avec succès.")
 }
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> gh-pages
