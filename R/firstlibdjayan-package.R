a6aa2195e75f70c5facc0864963c836bab6fac07
Djayan-D <daeron.djayan@gmail.com>
2025-02-28 14:12:41 +0100

    description package

---
+++ R/firstlibdjayan-package.R
@@ -0,0 +1,20 @@
+#' firstlibdjayan: Analyse et Visualisation des Données Communales et Départementales
+#'
+#' Ce package permet l'analyse et la visualisation des données relatives aux élus des communes
+#' et départements français. Il inclut des outils pour calculer des statistiques, générer des graphiques
+#' et structurer les données en classes `commune` et `departement`.
+#'
+#' @description
+#' Ce package fournit des fonctions pour manipuler les données relatives aux élus des communes
+#' et départements français. Il inclut des outils pour calculer des statistiques, générer des graphiques
+#' et structurer les données en classes `commune` et `departement`.
+#'
+#' @name firstlibdjayan
+#' @author
+#' Daëron Djayan [aut, cre] (<daeron.djayan@gmail.com>)
+#'
+#' @seealso
+#' \url{https://github.com/Djayan-D/firstlibdjayan/} pour accéder au code source du package.
+#' \url{https://djayan-d.github.io/firstlibdjayan/} pour consulter la documentation en ligne.
+#' \url{https://sites.google.com/view/master-ecap/accueil} pour plus d'informations sur le Master ECAP.
+"_PACKAGE"
